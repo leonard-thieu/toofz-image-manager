b85ca425252cdda81791598fce6110afddd891f1
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-02 17:09:37 -0400

    Bump version.

--- toofz.NecroDancer.ImageManager/Properties/AssemblyInfo.cs
+++ toofz.NecroDancer.ImageManager/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("toofz NecroDancer")]
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2016")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
