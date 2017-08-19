4e9cdaf8a7eed6c28440136340815edd0a63dec2
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-19 10:25:10 -0400

    Extract ImageFile into its own file.

---
+++ toofz.NecroDancer.ImageManager/ImageFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace toofz.NecroDancer.ImageManager
+{
+    sealed class ImageFile
+    {
+        public ImageFile(string baseName, int frameIndex, string type, string ext, Guid format, byte[] data)
+        {
+            FrameIndex = frameIndex;
+            Format = format;
+            Data = data;
+
+            Name = $"{baseName}_{frameIndex}_{type}{ext}";
+        }
+
+        public int FrameIndex { get; }
+        public Guid Format { get; }
+        public byte[] Data { get; }
+        public string Name { get; }
+    }
+}
--- toofz.NecroDancer.ImageManager/Program.cs
+++ toofz.NecroDancer.ImageManager/Program.cs
@@ -11,23 +11,6 @@ using toofz.NecroDancer.Data;
 
 namespace toofz.NecroDancer.ImageManager
 {
-    sealed class ImageFile
-    {
-        public ImageFile(string baseName, int frameIndex, string type, string ext, Guid format, byte[] data)
-        {
-            FrameIndex = frameIndex;
-            Format = format;
-            Data = data;
-
-            Name = $"{baseName}_{frameIndex}_{type}{ext}";
-        }
-
-        public int FrameIndex { get; }
-        public Guid Format { get; }
-        public byte[] Data { get; }
-        public string Name { get; }
-    }
-
     static class Program
     {
         static string DataDirectory;
